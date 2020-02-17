02a26e64c976c5f9c639e040ae2e8911675847c7
Loarlam <messiah02@mail.ru>
2020-02-17 20:24:20 +0300

    Add project files.

---
+++ Addition_task/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Addition_task
+{
+    static class Calculator
+    {
+        public static void CollectsMethod(this int variable1, int variable2)
+        {
+            AddUpTheNumbers(variable1, variable2);
+            if (variable1 < variable2)
+                SubstractNumbers(variable2, variable1);
+            else
+                SubstractNumbers(variable1, variable2);
+            if (variable2 != 0)
+            {
+                DivideNumbers(variable1, variable2);
+                DivideNumbersWithThRemainder(variable1, variable2);
+            }
+            else
+            {
+                Console.WriteLine("Деление на ноль запрещено!");
+            }
+        }
+
+        static void AddUpTheNumbers(int firstOperand, int secondOperand)
+        {
+            Console.WriteLine($"{firstOperand} + {secondOperand} = {firstOperand + secondOperand}");
+        }
+
+        static void SubstractNumbers(int firstOperand, int secondOperand)
+        {
+            Console.WriteLine($"{firstOperand} - {secondOperand} = {firstOperand - secondOperand}");
+        }
+
+        static void DivideNumbers(int firstOperand, int secondOperand)
+        {
+            Console.WriteLine($"{firstOperand} / {secondOperand} = {firstOperand / secondOperand}");
+        }
+
+        static void DivideNumbersWithThRemainder(int firstOperand, int secondOperand)
+        {
+            Console.WriteLine($"{firstOperand} % {secondOperand} = {firstOperand / secondOperand}.{firstOperand % secondOperand}");
+        }
+    }
+}
---
+++ Addition_task/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Используя Visual Studio, создайте проект по шаблону Console Application.  
+Требуется:  
+Создать статический класс Calculator, с методами для выполнения основных арифметических операций. 
+Написать программу, которая выводит на экран основные арифметические операции. 
+ */
+using System;
+
+namespace Addition_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Первое число = ");
+            int operand1 = Int32.Parse(Console.ReadLine());
+            Console.Write($"Второе число = ");
+            int operand2 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            operand1.CollectsMethod(operand2);
+
+            Console.ReadKey();
+        }
+    }
+}
